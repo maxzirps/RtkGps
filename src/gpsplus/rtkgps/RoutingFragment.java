c594b8d1beaa2f7ed6d66294c285428d7314e273
Maximilian Zirps <maximilian.zirps@gmail.com>
2020-03-17 22:27:32 +0100

    add info overlay

--- src/gpsplus/rtkgps/RoutingFragment.java
+++ src/gpsplus/rtkgps/RoutingFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -15,19 +16,25 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import gpsplus.rtkgps.routing.StatusOverlay;
 import gpsplus.rtkgps.view.GTimeView;
 
 public class RoutingFragment extends Fragment {
-    @BindView(R.id.map)
-    MapView map;
+    private MapView mMapView;
+    @BindView(R.id.map_container) ViewGroup mMapViewContainer;
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
@@ -37,6 +44,7 @@ public class RoutingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mMapView = new MapView(inflater.getContext());
         View v = inflater.inflate(R.layout.fragment_routing, container, false);
         ButterKnife.bind(this, v);
         //handle permissions first, before map is created. not depicted here
@@ -49,36 +57,55 @@ public class RoutingFragment extends Fragment {
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
         //see also StorageUtils
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
-        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx),map);
+        MyLocationNewOverlay mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx),mMapView);
         mLocationOverlay.enableMyLocation();
         mLocationOverlay.enableFollowLocation();
         // load a bitmap from the drawable folder
         Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.cursor);
 
-        Bitmap scaled = Bitmap.createScaledBitmap(b, 48, 48, false);
+        Bitmap scaled = Bitmap.createScaledBitmap(b, 48, 48, true);
         mLocationOverlay.setPersonIcon(scaled);
-        IMapController mapController = map.getController();
-        mapController.setZoom(18);
+        IMapController mapController = mMapView.getController();
+        mapController.setZoom(14);
         mapController.setCenter(mLocationOverlay.getMyLocation());
-        map.getOverlays().add(mLocationOverlay);
+        mMapView.getOverlays().add(mLocationOverlay);
 
-        CompassOverlay mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), map);
+        CompassOverlay mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), mMapView);
         mCompassOverlay.enableCompass();
-        map.getOverlays().add(mCompassOverlay);
+        mMapView.getOverlays().add(mCompassOverlay);
 
 
-        RotationGestureOverlay mRotationGestureOverlay = new RotationGestureOverlay(ctx, map);
-        mRotationGestureOverlay.setEnabled(true);
-        map.setMultiTouchControls(true);
-        map.getOverlays().add(mRotationGestureOverlay);
+        mMapView.setMultiTouchControls(true);
 
         //needed for pinch zooms
-        map.setMultiTouchControls(true);
+        mMapView.setMultiTouchControls(true);
 
         //scales tiles to the current screen's DPI, helps with readability of labels
-        map.setTilesScaledToDpi(true);
+        mMapView.setTilesScaledToDpi(true);
 
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        mMapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        Polyline line = new Polyline();   //see note below!
+        List<GeoPoint> pts = new ArrayList<>();
+
+
+        pts.add(new GeoPoint(48.08609227831706,16.284162998199463
+                ));
+        pts.add(new GeoPoint( 48.087195985118406,       16.28396987915039
+                ));
+        pts.add(new GeoPoint( 48.08835700177577,       16.283755302429
+                ));
+        pts.add(new GeoPoint( 48.08950365916834,       16.28339052200317
+                ));
+        pts.add(new GeoPoint( 48.091209264740755,       16.28349781036377
+                ));
+        line.setPoints(pts);
+        line.setColor(Color.RED);
+        mMapView.getOverlays().add(line);
+        mMapView.getOverlays().add(new StatusOverlay());
+
+        mMapViewContainer.addView(mMapView, 0);
+
         return v;
     }
 
@@ -88,7 +115,7 @@ public class RoutingFragment extends Fragment {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        mMapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
     public void onPause(){
@@ -97,6 +124,6 @@ public class RoutingFragment extends Fragment {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
-         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        mMapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 }
