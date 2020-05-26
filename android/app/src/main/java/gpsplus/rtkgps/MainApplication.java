ca6897fd1ef364e867a4e6529eb30482b971ef23
Maximilian Zirps <29249513+zirmax@users.noreply.github.com>
2020-05-26 08:32:27 +0200

    moved to new react-native structure

---
+++ android/app/src/main/java/gpsplus/rtkgps/MainApplication.java
@@ -0,0 +1,102 @@
+package gpsplus.rtkgps;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.StrictMode;
+import android.util.Log;
+
+// import com.airbnb.android.react.maps.MapsPackage;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import org.gdal.gdal.gdal;
+import org.gdal.ogr.ogr;
+import org.proj4.PJ;
+
+import java.util.Arrays;
+import java.util.List;
+
+import gpsplus.ntripcaster.NTRIPCaster;
+
+
+public class MainApplication extends Application implements ReactApplication {
+
+    private static final boolean DBG = BuildConfig.DEBUG & true;
+    private static String VERSION = "";
+
+    @Override
+    public void onCreate() {
+        if (DBG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+        super.onCreate();
+        //ACRA.init(this);
+        System.loadLibrary("proj");
+        Log.v("Proj4","Proj4 version: "+PJ.getVersion());
+
+        System.loadLibrary("ntripcaster");
+        Log.v("ntripcaster","NTRIP Caster "+NTRIPCaster.getVersion());
+
+        System.loadLibrary("rtkgps");
+
+        //System.loadLibrary("gdalalljni"); //Automaticaly done
+        ogr.RegisterAll();
+        gdal.AllRegister();
+        Log.v("GDAL",gdal.VersionInfo("--version"));
+        //set version
+        PackageInfo pi;
+        try {
+            pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            MainApplication.VERSION = pi.versionName;
+        } catch (NameNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String getVersion() {
+        return MainApplication.VERSION;
+    }
+
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage()
+                    //new MapsPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+}
--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+/**
+ * @format
+ */
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
-      </View>
-    );
-  }
-}
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-});
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent("MyReactNativeApp", () => HelloWorld);
+AppRegistry.registerComponent(appName, () => App);
